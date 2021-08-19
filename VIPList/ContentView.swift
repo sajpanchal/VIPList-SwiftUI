9d5acbd87fffa7f6dba096ae28eb1a5ee81a4cd5
Saj Panchal <sajpanchal@gmail.com>
2021-08-19 12:51:51 -0400

    added code to create coordinator in Picker Struct to respond to SwiftUI events and attach selected picture to swiftUI.

--- VIPList/ContentView.swift
+++ VIPList/ContentView.swift
@@ -9,19 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State var showingPicker = false
+    @State var image: Image?
+    //pass this image to ImagePicker.
+    @State var inputImage: UIImage?
     var body: some View {
         NavigationView {
             VStack {
-               
+                image?
+                    .resizable()
+                    .scaledToFit()
                 Button("Add Profile Pics") {
                     showingPicker = true
                 }
             }.navigationBarTitle("VIPList")
-            .sheet(isPresented: $showingPicker, content: {
-                ImagePicker()
+            .sheet(isPresented: $showingPicker, onDismiss: loadImage, content: {
+                // pass the empty UIImage instance to Picker.
+                //it will then return it with an selected image.
+                ImagePicker(image: $inputImage)
+                
             })
+            
            
         }
+        
+    }
+    //convert the UIImage to Image and assign it to our SwiftUI Image.
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        image = Image(uiImage: inputImage)
         
     }
 }
--- VIPList/ImagePicker.swift
+++ VIPList/ImagePicker.swift
@@ -9,10 +9,17 @@ import Foundation
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    // to dismiss the ImagePicker on selection of picture
+    @Environment(\.presentationMode) var presentationMode
+    // to attach the image selected to return it to SwiftUI content view.
+    @Binding var image: UIImage?
     
+    typealias UIViewControllerType = UIImagePickerController
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        // coordinator is set as a delegate to picker so that it will respond to SwiftUI events.
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -20,7 +27,24 @@ struct ImagePicker: UIViewControllerRepresentable {
         
     }
     
-    typealias UIViewControllerType = UIImagePickerController
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        // when image is selected.
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            // if the image is selected.
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage // set the image as UIImage of Picker property.
+            }
+            // dismiss the PickerController
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
     
     
 }
